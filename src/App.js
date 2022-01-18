538fd86b7bd2c25f968b3fdce22a5550b11fd4c5
Sagar Khadka <sagar.khadka@diagonal.software>
2022-01-18 23:07:20 +0545

    React Query and traditional methods comparison basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { SuperHeroesPage } from "./components/SuperHeroes.page";
+import { RQSuperHeroPage } from "./components/RQSuperHeroes.page";
+import { HomePage } from "./components/Home.page";
+import { QueryClientProvider, QueryClient } from "react-query";
 
+const queryClient = new QueryClient();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/super-heroes">
+                                    Traditional Super Heroes
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/rq-super-heroes">
+                                    RQ Super Heroes
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route path="/super-heroes">
+                            <SuperHeroesPage />
+                        </Route>
+                        <Route path="/rq-super-heroes">
+                            <RQSuperHeroPage />
+                        </Route>
+                        <Route path="/">
+                            <HomePage />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
